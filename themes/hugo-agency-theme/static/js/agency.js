87cade0d01e6f852ee8aa1fc9cfe96db94b7a3f4
Thomas Kluth <t.kluth@posteo.de>
2019-01-28 11:50:07 +0100

    fix #3; make phone not required

--- themes/hugo-agency-theme/static/js/agency.js
+++ themes/hugo-agency-theme/static/js/agency.js
@@ -46,21 +46,6 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Async contact form
-$('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(data, textStatus, jqXHR){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (jqXHR.status == 200) {
-      $('form[id=contactForm] #success').show();
-    }}, 'json').fail(function(){
-      $('form[id=contactForm] #success').hide();
-      $('form[id=contactForm] #error').hide();
-      $('form[id=contactForm] #error').show();
-  });
-  return false;
-});
-
 // Contact form validation
 $.validate({
   modules : 'html5, toggleDisabled'
